1860c0bf549353c68500f5aea2a469b919c7c5f6
Vinicius Guimarães <vg.guimaraes@hotmail.com>
2025-01-04 21:45:01 -0300

    feat: add gorm config

--- cmd/gocrud-server/main.go
+++ cmd/gocrud-server/main.go
@@ -3,10 +3,17 @@ package main
 import (
 	"net/http"
 
+	"github.com/VGuimaraes5/gocrud/internal/model"
+	"github.com/VGuimaraes5/gocrud/pkg/gorm/db"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	err := db.Init()
+	if err != nil {
+		panic("Error initializing database")
+	}
+
 	router := gin.Default()
 
 	api := router.Group("/api")
@@ -16,6 +23,24 @@ func main() {
 			v1.GET("/health", func(c *gin.Context) {
 				c.JSON(http.StatusOK, gin.H{
 					"message": "OK",
+				})
+			})
+
+			v1.GET("/users", func(ctx *gin.Context) {
+				db := db.GetConnection()
+				users := []model.User{}
+
+				if err := db.Find(&users).Error; err != nil {
+					ctx.Header("Content-type", "application/json")
+					ctx.JSON(http.StatusInternalServerError, gin.H{
+						"message": "error listing users",
+					})
+					return
+				}
+
+				ctx.Header("Content-type", "application/json")
+				ctx.JSON(http.StatusOK, gin.H{
+					"users": users,
 				})
 			})
 		}
---
+++ pkg/gorm/db/db.go
@@ -0,0 +1,33 @@
+package db
+
+import (
+	"github.com/VGuimaraes5/gocrud/internal/model"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	connection *gorm.DB
+)
+
+func Init() error {
+	dsn := "host=postgres-db user=postgres password=postgres dbname=postgres port=5432 sslmode=disable TimeZone=America/Sao_Paulo"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		println("Error connecting to database: %v", err)
+		return err
+	}
+
+	err = db.AutoMigrate(&model.User{})
+	if err != nil {
+		println("sqlite automigration error: %v", err)
+		return err
+	}
+
+	connection = db
+	return nil
+}
+
+func GetConnection() *gorm.DB {
+	return connection
+}
